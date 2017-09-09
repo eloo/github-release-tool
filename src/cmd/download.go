939da90137909f8ca6642591dd9629c991508484
Joseph Weigl <joseph.weigl@gmail.com>
2017-09-09 19:46:17 +0200

    Separate binary and checksum files

--- src/cmd/download.go
+++ src/cmd/download.go
@@ -8,11 +8,13 @@ import (
 	"github.com/urfave/cli"
 	"gopkg.in/resty.v0"
 	"os"
-	"sort"
+
 	"strings"
+	"robpike.io/filter"
 )
 
 const githubReleaseURLTemplate = "https://api.github.com/repos/%s/releases"
+const githubLatestReleaseURLTemplate = "https://api.github.com/repos/%s/releases/latest"
 
 var (
 	// Download command used from the cli
@@ -27,6 +29,9 @@ var (
 				Value: "",
 				Name:  "search, s",
 				Usage: "search string for filename matching",
+			},
+			cli.BoolFlag{
+				Name: "drafts, d",
 			}},
 		Action: func(c *cli.Context) error {
 			downloadRelease(c.Args().First(), c.String("search"))
@@ -35,40 +40,94 @@ var (
 	}
 )
 
-func downloadRelease(repository string, pattern string) {
-	releases := getReleases(repository)
-	sort.Slice(releases, func(i, j int) bool {
-		return releases[i].PublishedAt.Before(releases[i].PublishedAt)
-	})
-	//for _, release := range releases {
-	//	fmt.Println(release.TagName)
-	//}
-
-	var filteredAssets []models.Asset
-	if len(pattern) > 0 {
-		log.Info("Pattern found: %s", pattern)
-		for _, asset := range releases[0].Assets {
-			if strings.Contains(asset.Name, pattern) {
-				filteredAssets = append(filteredAssets, asset)
-			}
-		}
-	}
-	if len(filteredAssets) > 2 {
-		log.Error("found multiple candidate files")
-	}
-	for _, file := range filteredAssets {
-		fmt.Println(file.Name)
-	}
-	fmt.Println(filteredAssets[0].DownloadUrl)
+func downloadAsset(candidate models.DownloadCandidate){
 	resty.SetRedirectPolicy(resty.FlexibleRedirectPolicy(15))
 	response, err := resty.R().
-		SetOutput(filteredAssets[0].Name).
+		SetOutput(candidate.File.Name).
 		SetAuthToken(os.Getenv("GITHUB_TOKEN")).
-		Get(filteredAssets[0].DownloadUrl)
+		Get(candidate.File.DownloadUrl)
 	fmt.Println(response.RawResponse)
 	if err != nil {
 		fmt.Errorf("download failed: %s", err)
 	}
+}
+
+func downloadRelease(repository string, pattern string) {
+	latestRelease := getLatestRelease(repository)
+
+	//releases := getReleases(repository)
+	//sort.Slice(releases, func(i, j int) bool {
+	//	return releases[i].PublishedAt.Before(releases[i].PublishedAt)
+	//})
+
+	var assets []models.Asset
+	if len(pattern) > 0 {
+		log.Info("Pattern found: %s", pattern)
+		for _, asset := range latestRelease.Assets {
+			if strings.Contains(asset.Name, pattern) {
+				assets = append(assets, asset)
+			}
+		}
+	}
+	binaryAssets := getBinaries(assets)
+	checksumAssets := getChecksums(assets)
+	createDownloadCandidates(binaryAssets, checksumAssets)
+
+
+	//if len(filteredAssets) > 2 {
+	//	log.Error("found multiple candidate files")
+	//}
+	//for _, file := range filteredAssets {
+	//	fmt.Println(file.Name)
+	//}
+	//fmt.Println(filteredAssets[0].DownloadUrl)
+
+}
+func createDownloadCandidates(binaries []models.Asset, checksums []models.Asset) []models.DownloadCandidate {
+	var downloadCandidates []models.DownloadCandidate
+	for i := 0; i < len(binaries); i++ {
+		downloadCandidates = append(downloadCandidates, models.DownloadCandidate{File: binaries[i]})
+	}
+	return downloadCandidates
+}
+
+func getBinaries(assets []models.Asset) []models.Asset {
+	files := filter.Choose(assets, func(v models.Asset) bool {
+		return !strings.HasSuffix(v.Name, ".sha256")
+	})
+	binaries, ok := files.([]models.Asset)
+	if ok {
+		return binaries
+	} else {
+		return make([]models.Asset,0)
+	}
+}
+
+func getChecksums(assets []models.Asset) []models.Asset {
+	files := filter.Choose(assets, func(v models.Asset) bool {
+		return strings.HasSuffix(v.Name, ".sha256")
+	})
+	checksums, ok := files.([]models.Asset)
+	if ok {
+		return checksums
+	} else {
+		return make([]models.Asset,0)
+	}
+}
+
+func getLatestRelease(repository string) models.Release{
+	url := fmt.Sprintf(githubLatestReleaseURLTemplate, repository)
+	resp, err := resty.R().SetAuthToken(os.Getenv("GITHUB_TOKEN")).Get(url)
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+	var release models.Release
+	err = json.Unmarshal(resp.Body(), &release)
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+	log.Debug("%s: %s", release.TagName, release.PublishedAt.Local().String())
+	return release
 }
 
 func getReleases(repository string) []models.Release {
