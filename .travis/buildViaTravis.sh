076885903d359aeffa04ab71f51e187652915697
Unknown <joseph.weigl@gmail.com>
2018-04-21 17:59:44 +0200

    Add dep to make

--- .travis/buildViaTravis.sh
+++ .travis/buildViaTravis.sh
@@ -3,16 +3,16 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  make build
+  make dep build
 elif [ "$TRAVIS_BRANCH" == "master" ] ; then
   echo -e 'Build Master Branch ['$TRAVIS_BRANCH']'
-  make release
+  make dep release
 elif [ "$TRAVIS_BRANCH" == "develop" ] ; then
   echo -e 'Build Develop Branch ['$TRAVIS_BRANCH']'
-  make build
+  make dep build
 else
   echo -e 'Build brand ['$TRAVIS_BRANCH']'
-  make build
+  make dep build
 fi
 
 exit $EXIT
