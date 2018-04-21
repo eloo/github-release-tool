1196703b18f7e8a90a8d6d1434874ae3afe33e63
Unknown <joseph.weigl@gmail.com>
2018-04-21 16:17:39 +0200

    Use correct make goal

--- .travis/buildViaTravis.sh
+++ .travis/buildViaTravis.sh
@@ -3,16 +3,16 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  make goBuild
+  make build
 elif [ "$TRAVIS_BRANCH" == "master" ] ; then
   echo -e 'Build Master Branch ['$TRAVIS_BRANCH']'
-  make goBuild
+  make build
 elif [ "$TRAVIS_BRANCH" == "develop" ] ; then
   echo -e 'Build Develop Branch ['$TRAVIS_BRANCH']'
-  make goBuild
+  make build
 else
   echo -e 'Build brand ['$TRAVIS_BRANCH']'
-  make goBuild
+  make build
 fi
 
 exit $EXIT
