55d3fcc17f9bf0e276818998a04e35aeb6d37faa
Unknown <joseph.weigl@gmail.com>
2018-04-21 18:11:26 +0200

    Run tests in travis

--- .travis/buildViaTravis.sh
+++ .travis/buildViaTravis.sh
@@ -3,16 +3,16 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  make dep build
+  make dep test build
 elif [ "$TRAVIS_BRANCH" == "master" ] ; then
   echo -e 'Build Master Branch ['$TRAVIS_BRANCH']'
-  make dep release
+  make dep test release
 elif [ "$TRAVIS_BRANCH" == "develop" ] ; then
   echo -e 'Build Develop Branch ['$TRAVIS_BRANCH']'
-  make dep build
+  make dep test build
 else
   echo -e 'Build brand ['$TRAVIS_BRANCH']'
-  make dep build
+  make dep test	build
 fi
 
 exit $EXIT
